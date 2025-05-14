3fda4ef24bc16f8476e8b01d2febc39e7234807c
Panayiotis Halouvas <phalouvas@gmail.com>
2025-05-14 05:07:34 +0000

    feat: Replace 'status' field with 'disabled' and add validation logic

--- cognitive_folio/cognitive_folio/doctype/cf_portfolio/cf_portfolio.py
+++ cognitive_folio/cognitive_folio/doctype/cf_portfolio/cf_portfolio.py
@@ -8,11 +8,18 @@ from frappe.model.document import Document
 class CFPortfolio(Document):
     def validate(self):
         self.validate_start_date()
+        self.validate_disabled_state()
     
     def validate_start_date(self):
         """Ensure start date is not in future"""
         if self.start_date and self.start_date > frappe.utils.today():
             frappe.throw("Start Date cannot be in the future")
+    
+    def validate_disabled_state(self):
+        """Additional validations when disabling a portfolio"""
+        if self.disabled:
+            # Add any validations or actions needed when portfolio is disabled
+            pass
     
     def on_update(self):
         """Update linked records or perform other operations when portfolio is updated"""
