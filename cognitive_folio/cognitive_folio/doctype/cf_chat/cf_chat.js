1c840d16aaa181da5246a95fdc0cf967f7d09e3d
Panayiotis Halouvas <phalouvas@gmail.com>
2025-05-27 04:46:32 +0000

    feat: Implement custom button to add messages in CF Chat and update CF Chat Message creation timestamp

--- cognitive_folio/cognitive_folio/doctype/cf_chat/cf_chat.js
+++ cognitive_folio/cognitive_folio/doctype/cf_chat/cf_chat.js
@@ -1,8 +1,15 @@
 // Copyright (c) 2025, KAINOTOMO PH LTD and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("CF Chat", {
-// 	refresh(frm) {
-
-// 	},
-// });
+frappe.ui.form.on("CF Chat", {
+	refresh(frm) {
+		// Add custom button to quickly add new message
+		if (!frm.is_new()) {
+			frm.add_custom_button(__('Add Message'), function() {
+				frappe.new_doc('CF Chat Message', {
+					chat: frm.doc.name
+				});
+			});
+		}
+	}
+});
