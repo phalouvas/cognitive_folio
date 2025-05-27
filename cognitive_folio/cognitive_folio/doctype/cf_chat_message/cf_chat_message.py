061c57094caf1f09c4c854e182e118d3bee529b6
Panayiotis Halouvas <phalouvas@gmail.com>
2025-05-27 05:40:56 +0000

    feat: Add model selection and break sections in CF Chat Message doctype

--- cognitive_folio/cognitive_folio/doctype/cf_chat_message/cf_chat_message.py
+++ cognitive_folio/cognitive_folio/doctype/cf_chat_message/cf_chat_message.py
@@ -1,9 +1,35 @@
-# Copyright (c) 2025, KAINOTOMO PH LTD and contributors
-# For license information, please see license.txt
-
-# import frappe
+import frappe
 from frappe.model.document import Document
 
 
 class CFChatMessage(Document):
-	pass
+	
+	def before_insert(self):
+		
+		try:
+			from openai import OpenAI			
+		except ImportError:
+			frappe.throw("OpenAI package is not installed. Please run 'bench pip install openai'")
+			return 0
+		
+		chat = frappe.get_doc("CF Chat", self.chat)
+		settings = frappe.get_single("CF Settings")
+		client = OpenAI(api_key=settings.get_password('open_ai_api_key'), base_url=settings.open_ai_url)
+		
+		prompt = self.prompt
+
+		messages = [
+			{"role": "system", "content": chat.system_content if chat.system_content else "You are a helpful assistant."},
+			{"role": "user", "content": prompt}			
+		]
+
+		response = client.chat.completions.create(
+			model=self.model,
+			messages=messages,
+			stream=False,
+			temperature=0.2
+		)
+
+		self.response = response.choices[0].message.content if response.choices else "No response from OpenAI"
+		self.tokens = response.usage.to_json()
+		pass
