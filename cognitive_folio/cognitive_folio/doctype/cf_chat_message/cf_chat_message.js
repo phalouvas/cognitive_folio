f9d342b943df8123c3cd84b776e3624e07621088
Panayiotis Halouvas <phalouvas@gmail.com>
2025-05-29 04:34:55 +0000

    refactor: Remove realtime update listener from CF Chat Message

--- cognitive_folio/cognitive_folio/doctype/cf_chat_message/cf_chat_message.js
+++ cognitive_folio/cognitive_folio/doctype/cf_chat_message/cf_chat_message.js
@@ -1,15 +1,3 @@
 // Copyright (c) 2025, KAINOTOMO PH LTD and contributors
 // For license information, please see license.txt
 
-frappe.ui.form.on("CF Chat Message", {
-	
-	// Listen for realtime updates
-	onload(frm) {
-		frappe.realtime.on('chat_message_completed', (data) => {
-			if (data.message_id === frm.doc.name) {
-				// Reload the document to get latest data
-				frm.reload_doc();
-			}
-		});
-	}
-});
