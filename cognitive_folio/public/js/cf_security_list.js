f53dadd7009d3fa88810d44090a51c5cceabe0a9
Panayiotis Halouvas <phalouvas@gmail.com>
2025-06-01 04:04:33 +0000

    feat: Rename fetch_selected_data to fetch_data_selected and add generate_ai_suggestion_selected function

--- cognitive_folio/public/js/cf_security_list.js
+++ cognitive_folio/public/js/cf_security_list.js
@@ -13,19 +13,15 @@ frappe.listview_settings['CF Security'].onload = function(listview) {
             }
 
             frappe.call({
-                method: "cognitive_folio.cognitive_folio.doctype.cf_security.cf_security.fetch_selected_data",
+                method: "cognitive_folio.cognitive_folio.doctype.cf_security.cf_security.fetch_data_selected",
                 args: {
                     docnames: docnames,
                     with_fundamentals: false
                 },
                 freeze: true,
-                freeze_message: __("Fetching Latest Data..."),
+                freeze_message: __("Processing..."),
                 callback: function(r) {
                     if (!r.exc) {
-                        if (r.message && r.message.name) {
-                            frappe.set_route("Form", "CF Security", r.message.name);
-                        }
-                        
                         listview.refresh();
                     }
                 }
@@ -42,19 +38,39 @@ frappe.listview_settings['CF Security'].onload = function(listview) {
             }
 
             frappe.call({
-                method: "cognitive_folio.cognitive_folio.doctype.cf_security.cf_security.fetch_selected_data",
+                method: "cognitive_folio.cognitive_folio.doctype.cf_security.cf_security.fetch_data_selected",
+                args: {
+                    docnames: docnames
+                },
+                freeze: true,
+                freeze_message: __("Processing..."),
+                callback: function(r) {
+                    if (!r.exc) {
+                        listview.refresh();
+                    }
+                }
+            });
+        });
+
+        listview.page.add_action_item(__("Generate AI Suggestion"), function() {
+            const selected_docs = listview.get_checked_items();
+            const docnames = listview.get_checked_items(true);
+
+            if (selected_docs.length === 0) {
+                frappe.throw(__("Please select at least one CF Security"));
+                return;
+            }
+
+            frappe.call({
+                method: "cognitive_folio.cognitive_folio.doctype.cf_security.cf_security.generate_ai_suggestion_selected",
                 args: {
                     docnames: docnames,
                     with_fundamentals: true
                 },
                 freeze: true,
-                freeze_message: __("Fetching Latest Data..."),
+                freeze_message: __("Processing..."),
                 callback: function(r) {
                     if (!r.exc) {
-                        if (r.message && r.message.name) {
-                            frappe.set_route("Form", "CF Security", r.message.name);
-                        }
-                        
                         listview.refresh();
                     }
                 }
