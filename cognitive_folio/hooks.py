485d85f4f7243433cd2ffc70bfa62ab6bbd6331e
Panayiotis Halouvas <phalouvas@gmail.com>
2026-03-27 05:41:47 +0000

    feat: Update version to 16.4.2 and enhance dashboard configuration with new charts

--- cognitive_folio/hooks.py
+++ cognitive_folio/hooks.py
@@ -11,15 +11,15 @@ app_license = "mit"
 # required_apps = []
 
 # Each item in the list will be shown as an app in the apps page
-add_to_apps_screen = [
-    {
-        "name": "cognitive_folio",
-        "logo": "/assets/cognitive_folio/images/cognitive_folio_logo.svg",
-        "title": "Cognitive Folio",
-        "route": "/cognitive-folio"
+#add_to_apps_screen = [
+#    {
+#        "name": "cognitive_folio",
+#        "logo": "/assets/cognitive_folio/images/cognitive_folio_logo.svg",
+#        "title": "Cognitive Folio",
+#        "route": "/cognitive-folio"
 # 		"has_permission": "cognitive_folio.api.permission.has_app_permission"
-    }
-]
+#    }
+#]
 
 # Includes in <head>
 # ------------------
